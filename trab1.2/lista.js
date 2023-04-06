39c86edf7eb05ec8a8d2dbb307b8c7840134b730
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-04-06 13:49:52 -0300

    Add files via upload

--- trab1.2/lista.js
+++ trab1.2/lista.js
@@ -1,4 +1,4 @@
-const nameList = ["Aline", "Otavio", "Camila", "Nareba", "Jean", "Julebas", "Rany", "TI"]
+const nameList = ["Aline", "Otavio", "Cleiton", "Gabriel", "Jean", "Guilherme", "Ana", "Caio"]
 
 
 const botaofil = document.getElementById("Filtrar");
@@ -34,20 +34,44 @@ function inputHandler() {
 botaofil.addEventListener("click", inputHandler);
 
 function adicionarNome(){
-    nome = searchField.value.toLowerCase();
-    //adciona o nome ao final da lista
-    nameList.push(nome);
-    saida.innerHTML = "Nome adicionado " + nome 
+    nome = searchField.value.toLowerCase(); //pega o nome do local de pesquisa
+    if (nome != ''){
+        if (nameList.indexOf(nome) == -1){
+            //adciona o nome ao final da lista
+            nameList.push(nome);
+            saida.innerHTML = "Nome adicionado " + nome;
+        } else {
+            saida.innerHTML = "nome ja adicionado anteriormente"
+        }
+    
+    } else {
+        saida.innerHTML = "Insira um nome valido"
+    }
 
 }
 
-botaoadd.addEventListener("click", adicionarNome)
+botaoadd.addEventListener("click", adicionarNome);
 
 
 function removerNome(){
     nome = searchField.value.toLowerCase();
+
+    var index = nameList.indexOf(nome);
+    if (index > -1) {
+        nameList.splice(index, 1);
+        saida.innerHTML = "Nome removido: " + nome;
+      } else {
+        saida.innerHTML = 'Não foi possivel localizar o elemento "' + nome + '"'
+      }
     
 }
+
+botaorel.addEventListener("click", removerNome);
+
+    
+
+
+
 
 
 
