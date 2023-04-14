c708a11b51c1bef7cb2f91a55f30ee77138015a7
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-04-14 11:28:10 -0300

    Add files via upload

--- trab1.2/lista.js
+++ trab1.2/lista.js
@@ -1,5 +1,8 @@
 const nameList = ["Aline", "Otavio", "Cleiton", "Gabriel", "Jean", "Guilherme", "Ana", "Caio"]
 
+const nomeAux = nameList;
+
+var num_seleciona = -1;
 
 const botaofil = document.getElementById("Filtrar");
 const botaoadd = document.getElementById("add");
@@ -15,7 +18,14 @@ this.fillList();
 function fillList(list = nameList) {
     listEl.innerHTML = "";
     for(let i = 0; i < list.length; i++) {
+
         let listItems = document.createElement("li");
+
+        if (num_seleciona == i){
+            listItems.className = "alert alert-warning";
+        } else{
+            listItems.className = "";
+        }
         listItems.innerHTML = list[i];
         listEl.appendChild(listItems);
     }
@@ -34,6 +44,7 @@ function inputHandler() {
 botaofil.addEventListener("click", inputHandler);
 
 function adicionarNome(){
+    num_seleciona = 0;
     nome = searchField.value.toLowerCase(); //pega o nome do local de pesquisa
     if (nome != ''){
         if (nameList.indexOf(nome) == -1){
@@ -54,6 +65,7 @@ botaoadd.addEventListener("click", adicionarNome);
 
 
 function removerNome(){
+    num_seleciona = 0;
     nome = searchField.value.toLowerCase();
 
     var index = nameList.indexOf(nome);
@@ -69,10 +81,28 @@ function removerNome(){
 botaorel.addEventListener("click", removerNome);
 
     
+function seleciona(){
+
+    var aux = nameList[num_seleciona]
+
+    if (nameList.length == num_seleciona){
+        num_seleciona = 0;
+    } else{
+
+    num_seleciona += 1;
+
+    }
+
+    console.log(num_seleciona);
+
+
+    inputHandler();
 
 
 
 
+}
 
+botaosel.addEventListener("click", seleciona)
 
 
